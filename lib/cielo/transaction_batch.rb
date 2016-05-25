ffc0e03592852ba505703664a73f8a957db3a45b
Gabriel Rios <gabrielfalcaorios@gmail.com>
2016-05-25 16:11:52 -0300

    Fix batch xml format

--- lib/cielo/transaction_batch.rb
+++ lib/cielo/transaction_batch.rb
@@ -1,19 +1,27 @@
 module Cielo
   class TransactionBatch
-    def initialize(transactions)
+    def initialize(batch_number, operation, transactions)
+      @batch_number = batch_number
+      @operation = operation
       @connection = Cielo::Connection.new
       @transactions = transactions
     end
 
-    attr_reader :connection, :transactions
+    attr_reader :connection, :transactions, :batch_number, :operation
 
     def xml
       build_xml.target!
     end
 
+    def create!
+      @connection.make_request!(build_xml)
+    end
+
     def build_xml
-      connection.xml_builder('upload-lote') do |xml, target|
-        if target == :after
+      connection.xml_builder('requisicao-lote') do |xml, target|
+        xml.tag!('numero-lote', batch_number)
+        xml.tag!('tipo-operacao', operation)
+        xml.tag!('lista-requisicoes') do
           transactions.each do |transaction|
             xml << transaction.xml
           end
