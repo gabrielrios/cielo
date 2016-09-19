f61f396408df28c7169369c282a0c0cb3f04fe79
Gabriel Rios <gabrielfalcaorios@gmail.com>
2016-09-19 14:44:10 -0300

    Add description to pedido

--- lib/cielo/transaction.rb
+++ lib/cielo/transaction.rb
@@ -106,7 +106,7 @@ module Cielo
 
     def default_transaction_xml(xml)
       xml.tag!('dados-pedido') do
-        [:numero, :valor, :moeda, :"data-hora", :idioma, :"soft-descriptor"].each do |key|
+        [:numero, :valor, :moeda, :"data-hora", :descricao, :idioma, :"soft-descriptor"].each do |key|
           xml.tag!(key.to_s, parameters[key].to_s)
         end
       end
